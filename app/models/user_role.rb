9c24ba1d7fb681e36b22c53950aaf3617286c2b4
jeffoty <odhiambo47@gmail.com>
2023-06-29 03:09:21 +0300

    📦 user and roles join table.

---
+++ app/models/user_role.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: user_roles
+#
+#  id         :bigint           not null, primary key
+#  user_id    :bigint           not null
+#  role_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_user_roles_on_role_id  (role_id)
+#  index_user_roles_on_user_id  (user_id)
+#
+class UserRole < ApplicationRecord
+  belongs_to :user
+  belongs_to :role
+end
